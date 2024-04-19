f0ae7157e3e9011ae2feddbcb40724b76293cb59
DhruvPaghdal <dhruvpaghdal2@gmail.com>
2024-04-19 16:25:16 +0530

    Added Spinner for login

--- payroll-client/src/components/login/loginForm.js
+++ payroll-client/src/components/login/loginForm.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import { useForm } from "react-hook-form";
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
@@ -9,20 +9,25 @@ import PayrollContext from '../../context/payrollContext';
 import { loginEnum } from '../../constValue';
 import { login } from '../../services/indexService';
 import { useNavigate } from "react-router-dom";
+import Spinner from 'react-bootstrap/Spinner';
 
 const LoginForm = () => {
   const navigate = useNavigate();
   const context = useContext(PayrollContext);
+  const [spinnerVisiblity, setSpinnerVisiblity] = useState("d-none");
   const {setLoginCard, setAlertData, setAlertShow, setAlertVariant} = context;
   const { register, handleSubmit, formState: { errors }} = useForm();
   const onSubmit = async(payload) => {
+    setSpinnerVisiblity("visible");
     const { success, status, data, message} = await login(payload);
     if(!success) {
+      setSpinnerVisiblity("d-none");
       setAlertVariant("danger");
       setAlertShow(true);
       setAlertData(message);
     }
     else{
+      setSpinnerVisiblity("d-none");
       localStorage.setItem("token", data);
       navigate("/home/dashboard");
     }
@@ -47,8 +52,13 @@ const LoginForm = () => {
               <p style={{fontSize: "13px", textAlign: "left", color: "#6c8080"}}> {errors.password && errors.password.message}</p>
               <p className='py-3 m-0 text-end hoverEffect' style={{fontWeight: "bold",color: "#0d6efd"}} onClick={() => {setLoginCard(loginEnum.forgot_password)}}>Forgot Password?</p>
           </Form.Group>
+          <div className='text-center pb-3'>
+            <Spinner animation="border" role="status" variant="primary" className={spinnerVisiblity}>
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          </div>
           <div >
-              <Button className='w-100' type='submit' >Login</Button>
+            <Button className='w-100' type='submit' >Login</Button>
           </div>
         </Form>
       </Card.Text>
